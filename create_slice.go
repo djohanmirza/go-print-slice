bd71fb3e7b590242499fdfeabdbcc7ad9bc42fff
Mirza Djohan <mirzadjohan@gmail.com>
2025-02-20 14:20:57 +0700

    baru

---
+++ create_slice.go
@@ -0,0 +1,9 @@
+package print_slice
+
+func CreateSlice(n int) (result [][]int) {
+	result = make([][]int, n)
+	for i := 0; i < n; i++ {
+		result[i] = make([]int, n)
+	}
+	return result
+}
---
+++ go_print.go
@@ -0,0 +1,15 @@
+package print_slice
+
+import "fmt"
+
+func Print(slice []int) {
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i])
+	}
+}
+
+func Print2D(slice [][]int) {
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i])
+	}
+}
